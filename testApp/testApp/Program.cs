518db8fa7a610eb58e6c1a7c3b78eff7bf465741
Srikanth Yadake <yadsri01@CSGICORP.COM>
2019-08-08 12:32:39 +0530

    test

---
+++ testApp/testApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("will be committed to git");
+        }
+    }
+}
